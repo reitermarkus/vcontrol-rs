fb9a75069b9384134c4fd1b274f6c10393349172
Markus Reiter <me@reitermark.us>
2024-03-22 20:37:23 +0100

    Add incomplete implementation.

---
+++ codegen/nrbf/src/length_prefixed_string.rs
@@ -0,0 +1,113 @@
+use std::str;
+
+use nom::{
+  bytes::complete::take,
+  combinator::map_res,
+  number::complete::u8,
+  sequence::{pair, preceded},
+  IResult,
+};
+
+fn parse_len(mut input: (&[u8], usize)) -> IResult<(&[u8], usize), u32> {
+  use nom::bits::complete::{bool, tag, take};
+
+  let mut len = 0;
+
+  let (mut parse_next, mut len_part): (bool, u8);
+  (input, (parse_next, len_part)) = pair(bool, take(7usize))(input)?;
+  len |= len_part as u32;
+
+  if parse_next {
+    (input, (parse_next, len_part)) = pair(bool, take(7usize))(input)?;
+    len |= (len_part as u32) << 7;
+
+    if parse_next {
+      (input, (parse_next, len_part)) = pair(bool, take(7usize))(input)?;
+      len |= (len_part as u32) << 14;
+
+      if parse_next {
+        (input, (parse_next, len_part)) = pair(bool, take(7usize))(input)?;
+        len |= (len_part as u32) << 21;
+
+        if parse_next {
+          (input, len_part) = preceded(tag(0b00000, 5usize), take(3usize))(input)?;
+          len |= (len_part as u32) << 28;
+        }
+      }
+    }
+  }
+
+  Ok((input, len))
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct LengthPrefixedString<'s> {
+  pub string: &'s str,
+}
+
+impl<'i> LengthPrefixedString<'i> {
+  pub fn parse(mut input: &'i [u8]) -> IResult<&'i [u8], Self> {
+    let ((input, _), len) = parse_len((input, 0)).map_err(|err| err.map_input(|(input, _)| input))?;
+    dbg!(len);
+    let (input, string) = map_res(take(len), str::from_utf8)(input)?;
+
+    Ok((input, Self { string }))
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use std::iter::repeat;
+
+  use super::LengthPrefixedString;
+
+  #[test]
+  fn length_127() {
+    let string = repeat('a').take(127).collect::<String>();
+
+    let mut input = vec![0b01111111];
+    input.extend(string.as_bytes());
+
+    assert_eq!(LengthPrefixedString::parse(&input), Ok(([].as_slice(), LengthPrefixedString { string: &string })));
+  }
+  #[test]
+  fn length_16383() {
+    let string = repeat('a').take(16383).collect::<String>();
+
+    let mut input = vec![0b11111111, 0b01111111];
+    input.extend(string.as_bytes());
+
+    assert_eq!(LengthPrefixedString::parse(&input), Ok(([].as_slice(), LengthPrefixedString { string: &string })));
+  }
+
+  #[test]
+  fn length_2097151() {
+    let string = repeat('a').take(2097151).collect::<String>();
+
+    let mut input = vec![0b11111111, 0b11111111, 0b01111111];
+    input.extend(string.as_bytes());
+
+    assert_eq!(LengthPrefixedString::parse(&input), Ok(([].as_slice(), LengthPrefixedString { string: &string })));
+  }
+
+  #[test]
+  fn length_268435455() {
+    let string = repeat('a').take(268435455).collect::<String>();
+
+    let mut input = vec![0b11111111, 0b11111111, 0b11111111, 0b01111111];
+    input.extend(string.as_bytes());
+
+    assert_eq!(LengthPrefixedString::parse(&input), Ok(([].as_slice(), LengthPrefixedString { string: &string })));
+  }
+
+  #[ignore = "needs too much memory"]
+  #[test]
+  fn length_2147483647() {
+    let string = repeat('a').take(2147483647).collect::<String>();
+
+    let mut input = vec![0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b00000111];
+    input.extend(string.as_bytes());
+
+    assert_eq!(LengthPrefixedString::parse(&input), Ok(([].as_slice(), LengthPrefixedString { string: &string })));
+  }
+}
