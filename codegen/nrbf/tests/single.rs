03841c7321513aab41e9daa6ecf4a309c51c7586
Markus Reiter <me@reitermark.us>
2024-03-31 09:02:40 +0200

    Add more tests.

--- codegen/nrbf/tests/single.rs
+++ codegen/nrbf/tests/single.rs
@@ -1,32 +1,32 @@
 use const_str::concat_bytes;
 use nrbf::{
   common::{AdditionalTypeInfo, ClassInfo, MemberTypeInfo},
-  data_type::{Boolean, Int32, LengthPrefixedString},
+  data_type::{Int32, LengthPrefixedString, Single},
   enumeration::{BinaryType, PrimitiveType},
   grammar::{Class, Classes, MemberReference2, MemberReferenceInner, Referenceable, RemotingMessage},
   record::{MemberPrimitiveUnTyped, MessageEnd, SerializationHeader, SystemClassWithMembersAndTypes},
 };
 
-#[rustfmt::skip]
-const INPUT: &[u8] = concat_bytes!(
-  0,
-    b"\x01\x00\x00\x00",
-    b"\xFF\xFF\xFF\xFF",
-    b"\x01\x00\x00\x00",
-    b"\x00\x00\x00\x00",
-  4,
-    b"\x01\x00\x00\x00",
-    14, "System.Boolean",
-    b"\x01\x00\x00\x00",
-    7, "m_value",
-    0,
-    1,
-    0x01,
-  11
-);
-
 #[test]
-fn boolean() {
+fn single() {
+  #[rustfmt::skip]
+  let input = concat_bytes!(
+    0,
+      b"\x01\x00\x00\x00",
+      b"\xFF\xFF\xFF\xFF",
+      b"\x01\x00\x00\x00",
+      b"\x00\x00\x00\x00",
+    4,
+      b"\x01\x00\x00\x00",
+      13, "System.Single",
+      b"\x01\x00\x00\x00",
+      7, "m_value",
+      0,
+      11,
+      b"\xc3\xf5\x48\x40",
+    11
+  );
+
   let output = RemotingMessage {
     header: SerializationHeader {
       root_id: Int32(1),
@@ -39,17 +39,17 @@ fn boolean() {
       class: Class::SystemClassWithMembersAndTypes(SystemClassWithMembersAndTypes {
         class_info: ClassInfo {
           object_id: Int32(1),
-          name: LengthPrefixedString::from("System.Boolean"),
+          name: LengthPrefixedString::from("System.Single"),
           member_names: vec![LengthPrefixedString::from("m_value")],
         },
         member_type_info: MemberTypeInfo {
           binary_type_enums: vec![BinaryType::Primitive],
-          additional_infos: vec![Some(AdditionalTypeInfo::Primitive(PrimitiveType::Boolean))],
+          additional_infos: vec![Some(AdditionalTypeInfo::Primitive(PrimitiveType::Single))],
         },
       }),
       member_references: vec![MemberReference2 {
         binary_library: None,
-        member_reference: MemberReferenceInner::MemberPrimitiveUnTyped(MemberPrimitiveUnTyped::Boolean(Boolean(true))),
+        member_reference: MemberReferenceInner::MemberPrimitiveUnTyped(MemberPrimitiveUnTyped::Single(Single(3.14))),
       }],
     })],
     method_call_or_return: None,
@@ -57,11 +57,5 @@ fn boolean() {
     end: MessageEnd,
   };
 
-  assert_eq!(RemotingMessage::parse(INPUT), Ok(([].as_slice(), output)));
-}
-
-#[cfg(feature = "serde")]
-#[test]
-fn int32_deserialize() {
-  assert_eq!(nrbf::from_stream(INPUT), Ok(true));
+  assert_eq!(RemotingMessage::parse(input), Ok(([].as_slice(), output)));
 }
