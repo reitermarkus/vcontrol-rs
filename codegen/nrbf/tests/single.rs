ed340b67fa4daf826f57806d12fe3f0f3d3cfcda
Markus Reiter <me@reitermark.us>
2024-03-31 09:05:19 +0200

    Add test.

--- codegen/nrbf/tests/single.rs
+++ codegen/nrbf/tests/single.rs
@@ -7,26 +7,26 @@ use nrbf::{
   record::{MemberPrimitiveUnTyped, MessageEnd, SerializationHeader, SystemClassWithMembersAndTypes},
 };
 
+#[rustfmt::skip]
+const INPUT: &[u8] = concat_bytes!(
+  0,
+    b"\x01\x00\x00\x00",
+    b"\xFF\xFF\xFF\xFF",
+    b"\x01\x00\x00\x00",
+    b"\x00\x00\x00\x00",
+  4,
+    b"\x01\x00\x00\x00",
+    13, "System.Single",
+    b"\x01\x00\x00\x00",
+    7, "m_value",
+    0,
+    11,
+    b"\xc3\xf5\x48\x40",
+  11
+);
+
 #[test]
 fn single() {
-  #[rustfmt::skip]
-  let input = concat_bytes!(
-    0,
-      b"\x01\x00\x00\x00",
-      b"\xFF\xFF\xFF\xFF",
-      b"\x01\x00\x00\x00",
-      b"\x00\x00\x00\x00",
-    4,
-      b"\x01\x00\x00\x00",
-      13, "System.Single",
-      b"\x01\x00\x00\x00",
-      7, "m_value",
-      0,
-      11,
-      b"\xc3\xf5\x48\x40",
-    11
-  );
-
   let output = RemotingMessage {
     header: SerializationHeader {
       root_id: Int32(1),
@@ -57,5 +57,11 @@ fn single() {
     end: MessageEnd,
   };
 
-  assert_eq!(RemotingMessage::parse(input), Ok(([].as_slice(), output)));
+  assert_eq!(RemotingMessage::parse(INPUT), Ok(([].as_slice(), output)));
+}
+
+#[cfg(feature = "serde")]
+#[test]
+fn single_deserialize() {
+  assert_eq!(nrbf::from_stream(INPUT), Ok(3.14f32));
 }
