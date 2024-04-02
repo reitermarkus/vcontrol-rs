059acc2d48ac30546da74d723fbc155536073bd5
Markus Reiter <me@reitermark.us>
2024-04-02 20:18:23 +0200

    Clean up modules.

--- codegen/nrbf/tests/method_return.rs
+++ codegen/nrbf/tests/method_return.rs
@@ -2,7 +2,7 @@ use std::collections::BTreeMap;
 
 use nrbf::{
   data_type::{Int32, LengthPrefixedString},
-  grammar::{MethodCallOrReturn, MethodReturn, RemotingMessage},
+  grammar::{MethodCallOrReturn, RemotingMessage},
   method_invocation::{AnyValueWithCode, MessageFlags, StringValueWithCode},
   record::{BinaryMethodReturn, MessageEnd, SerializationHeader},
 };
@@ -24,16 +24,13 @@ fn method_return() {
       minor_version: Int32(0),
     },
     objects: BTreeMap::new(),
-    method_call_or_return: Some(MethodCallOrReturn::MethodReturn(MethodReturn {
-      binary_method_return: BinaryMethodReturn {
-        message_enum: MessageFlags::NO_ARGS | MessageFlags::NO_CONTEXT | MessageFlags::RETURN_VALUE_INLINE,
-        return_value: Some(AnyValueWithCode::String(StringValueWithCode::from(LengthPrefixedString::from(
-          "Address received",
-        )))),
-        call_context: None,
-        args: None,
-      },
-      return_call_array: None,
+    method_call_or_return: Some(MethodCallOrReturn::MethodReturn(BinaryMethodReturn {
+      message_enum: MessageFlags::NO_ARGS | MessageFlags::NO_CONTEXT | MessageFlags::RETURN_VALUE_INLINE,
+      return_value: Some(AnyValueWithCode::String(StringValueWithCode::from(LengthPrefixedString::from(
+        "Address received",
+      )))),
+      call_context: None,
+      args: None,
     })),
     end: MessageEnd,
   };
