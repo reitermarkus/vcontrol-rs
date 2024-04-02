1516b9c7e19dd8ca47ddd6eea9fef39bb9fc91d4
Markus Reiter <me@reitermark.us>
2024-04-02 11:16:15 +0200

    Add `class_info` methods.

--- codegen/nrbf/src/record/class_with_members_and_types.rs
+++ codegen/nrbf/src/record/class_with_members_and_types.rs
@@ -26,7 +26,12 @@ impl<'i> ClassWithMembersAndTypes<'i> {
   }
 
   #[inline]
+  pub fn class_info(&self) -> &ClassInfo<'i> {
+    &self.class_info
+  }
+
+  #[inline]
   pub(crate) fn object_id(&self) -> Int32 {
-    self.class_info.object_id()
+    self.class_info().object_id()
   }
 }
--- codegen/nrbf/src/record/system_class_with_members.rs
+++ codegen/nrbf/src/record/system_class_with_members.rs
@@ -18,6 +18,11 @@ impl<'i> SystemClassWithMembers<'i> {
   }
 
   #[inline]
+  pub fn class_info(&self) -> &ClassInfo<'i> {
+    &self.class_info
+  }
+
+  #[inline]
   pub(crate) fn object_id(&self) -> Int32 {
     self.class_info.object_id()
   }
