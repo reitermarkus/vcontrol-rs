f85d69f3ecc2b64a2fddbf6292b4f3d873129d35
Markus Reiter <me@reitermark.us>
2025-03-08 23:18:37 +0100

    Fix `scan` example.

--- examples/scan.rs
+++ examples/scan.rs
@@ -1,26 +1,24 @@
-use std::{
-  collections::BTreeMap,
-  env,
-  error::Error,
-  fs::OpenOptions,
-  io::{Seek, Write},
-};
+use std::{collections::BTreeMap, env, error::Error, fs::OpenOptions, io::Write};
 
-use vcontrol::{AccessMode, Command, DataType, Device, Optolink, Protocol, RawType, Value};
+use vcontrol::{Optolink, Protocol};
 
 // Scan all possible addresses and save their values in `scan-cache.yml`.
 // Helpful for finding addresses for undocumented event types.
-fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
   env_logger::init();
 
   let optolink_port = env::args().nth(1).expect("no serial port specified");
-  let mut optolink =
-    if optolink_port.contains(':') { Optolink::connect(optolink_port) } else { Optolink::open(optolink_port) }?;
+  let mut optolink = if optolink_port.contains(':') {
+    Optolink::connect(optolink_port).await
+  } else {
+    Optolink::open(optolink_port).await
+  }?;
 
   let mut file = OpenOptions::new().read(true).append(true).create(true).open("scan-cache.yml")?;
   let cache: BTreeMap<u16, u8> = serde_yaml::from_reader(&mut file).unwrap_or_default();
 
-  let protocol = Protocol::detect(&mut optolink).unwrap();
+  let protocol = Protocol::detect(&mut optolink).await.unwrap();
 
   for i in 0..u16::MAX {
     print!("\r{}/{}", i, u16::MAX);
@@ -33,14 +31,14 @@ fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     loop {
       let addr = i as u16;
       let mut buf = [0];
-      match protocol.get(&mut optolink, addr, &mut buf) {
+      match protocol.get(&mut optolink, addr, &mut buf).await {
         Ok(()) => {
           let value = buf[0];
           writeln!(file, "0x{:04X}: {}", addr, value)?;
         },
         Err(e) => {
           eprintln!("Error: {}", e);
-          protocol.negotiate(&mut optolink)?;
+          protocol.negotiate(&mut optolink).await?;
           continue
         },
       }
