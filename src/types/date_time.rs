066d7b93863d300e4e7ae31ecc2d4b35f4047afa
Markus Reiter <me@reitermark.us>
2025-04-18 14:42:44 +0200

    Update edition to 2024.

--- src/types/date_time.rs
+++ src/types/date_time.rs
@@ -65,8 +65,8 @@ impl JsonSchema for Date {
     "Date".into()
   }
 
-  fn json_schema(gen: &mut schemars::gen::SchemaGenerator) -> schemars::schema::Schema {
-    let mut schema = gen.subschema_for::<String>().into_object();
+  fn json_schema(generator: &mut schemars::r#gen::SchemaGenerator) -> schemars::schema::Schema {
+    let mut schema = generator.subschema_for::<String>().into_object();
     schema.format = Some("date".into());
     schema.into()
   }
@@ -147,8 +147,8 @@ impl JsonSchema for DateTime {
     "DateTime".into()
   }
 
-  fn json_schema(gen: &mut schemars::gen::SchemaGenerator) -> schemars::schema::Schema {
-    let mut schema = gen.subschema_for::<String>().into_object();
+  fn json_schema(generator: &mut schemars::r#gen::SchemaGenerator) -> schemars::schema::Schema {
+    let mut schema = generator.subschema_for::<String>().into_object();
     schema.format = Some("date-time".into());
     schema.into()
   }
